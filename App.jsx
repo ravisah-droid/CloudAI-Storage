da6d39d2e62857c01391bfbf4c4612af1b25fafd
ravisah-droid <ravisah3731@gmail.com>
2025-08-30 05:46:17 +0000

    update

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import NavAi from './AllTopic/01_Components/NavAi';
+import  FootAi  from './AllTopic/01_Components/FootAi';
+import Layout from './AllTopic/01_Components/Layout';
+
+function App() {
+  return (
+      <div >
+
+      <NavAi />
+      <Layout/>
+      <FootAi />
+    </div>
+
+   
+  );
+}
+
+  export default App;
